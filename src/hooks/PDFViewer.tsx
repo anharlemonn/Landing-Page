e50c0e00f49e7524dd080b031cbbec0f5f28f5cf
Anlon Zhu <zhu.anlon@gmail.com>
2024-08-11 21:19:39 -0700

    maybe pdf works now

--- src/hooks/PDFViewer.tsx
+++ src/hooks/PDFViewer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Worker, Viewer, ThemeContext as PDFThemeContext } from "@react-pdf-viewer/core";
+import { Viewer, Worker, ThemeContext as PDFThemeContext } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin, ToolbarProps, ToolbarSlot } from "@react-pdf-viewer/default-layout";
 import { themePlugin } from "@react-pdf-viewer/theme";
 import "@react-pdf-viewer/core/lib/styles/index.css";
@@ -13,7 +13,6 @@ interface PDFViewerProps {
 const PDFViewer: React.FC<PDFViewerProps> = ({ fileUrl }) => {
   const themePluginInstance = themePlugin();
   const { theme } = useTheme();
-  const { SwitchThemeButton } = themePluginInstance;
 
   const renderToolbar = (Toolbar: (props: ToolbarProps) => React.ReactElement) => (
     <Toolbar>
@@ -54,15 +53,15 @@ const PDFViewer: React.FC<PDFViewerProps> = ({ fileUrl }) => {
                     height: '100%',
                 }}
             >
-      <div style={{ flexGrow: 1, overflow: "auto"}}>
-        <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+       <div style={{ flexGrow: 1, overflow: "auto" }}>
+       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js">
           <Viewer
             fileUrl={fileUrl}
             plugins={[defaultLayoutPluginInstance, themePluginInstance]}
             theme={theme}
           />
-        </Worker>
-      </div>
+          </Worker>
+        </div>
       </div>
     </PDFThemeContext.Provider>
   );
